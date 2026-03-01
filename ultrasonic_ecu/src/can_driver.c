a67d16610b3b4b6b5989d9c620a5f07441df47b0
Naga Viswanth <132557170+nagabonthu@users.noreply.github.com>
2026-03-01 13:15:21 +0530

    Refactor CAN2 driver implementation

--- ultrasonic_ecu/src/can_driver.c
+++ ultrasonic_ecu/src/can_driver.c
@@ -1,74 +1,52 @@
-/** cn transmit****/
-/*can2_driver.c*/
-typedef unsigned int u32;
+/** CAN2 Transmit Driver Implementation **/
+/* can2_driver.c */
+
+typedef unsigned int u32;   // typedef for 32-bit unsigned type 
+
+// Function prototypes
 void can2_init(void);
 void delay_millisec(unsigned int);
+
+// CAN2 message structure
 typedef struct CAN2
-  {
-	u32 id;
-	u32 rtr;
-	u32 dlc;
-	u32 byteA;
-	u32 byteB;
- }CAN2_MSG;
-void can2_tx(CAN2_MSG m1);
-void can2_rx(CAN2_MSG*);
+{
+    u32 id;      // Message ID
+    u32 rtr;     // Remote Transmission Request (0 = data frame, 1 = remote frame)
+    u32 dlc;     // Data Length Code (number of bytes in message)
+    u32 byteA;   // First data word (lower 4 bytes)
+    u32 byteB;   // Second data word (upper 4 bytes)
+} CAN2_MSG;
+
+// Function to initialize CAN2 controller
 void can2_init(void)
- {
-	PINSEL1|=0x00014000;//P0.23-->RD2 & P0.24-->TD2
-	VPBDIV=1; //PCLK=60MHz
-    C2MOD=0x1; //CAN2 into Reset  Mode 
-	C2BTR=0x001C001D; //B125Kbps @ PLCK=60MHz	
-	AFMR=0x2;//Enable Acceptance Filter
-    C2MOD=0x0; //CAN2 into Normal Mode
- }
+{
+    PINSEL1 |= 0x00014000;   // Configure P0.23 as RD2 (receive), P0.24 as TD2 (transmit)
+    VPBDIV = 1;              // Set PCLK = 60 MHz
+    C2MOD = 0x1;             // Reset Mode
+    C2BTR = 0x001C001D;      // speed: 125 kbps @ 60 MHz PCLK
+    AFMR  = 0x2;             // Enable Acceptance Filter 
+    C2MOD = 0x0;             // CAN2 into Normal Mode (ready for communication)
+}
+
+// Function to transmit a CAN2 message
 void can2_tx(CAN2_MSG m1)
 {
-  C2TID1=m1.id;
-  C2TFI1=(m1.dlc<<16);	
-	if(m1.rtr==0) //if data frame
-	{
-		C2TFI1&=~(1<<30);//RTR=0
-		C2TDA1=m1.byteA; //lower 4bytes of data
-		C2TDB1=m1.byteB; //upper 4bytes of data
-	}
-	else
-	{
-		C2TFI1|=(1<<30); //RTR=1
-	}
-	C2CMR=(1<<0)|(1<<5);//Start Xmission & select Tx Buf1
-    while((C2GSR&(1<<3))==0);//wait for data Xmission.
-}
-void can2_rx(CAN2_MSG* m1)
-  {
-    while((C2GSR&0x1)==0);//waiting untill message received
-    m1->id=C2RID;
-	m1->dlc=(C2RFS>>16)&0xF;
-	m1->rtr=(C2RFS>>30)&0x1;
-	if(m1->rtr==0){ //if data frame
-		m1->byteA=C2RDA;
-		m1->byteB=C2RDB;
-	}
-   C2CMR=(1<<2);//free receiver buffer(imp)
-   }
+    C2TID1 = m1.id;          // Load message ID to transmit buffer
+    C2TFI1 = (m1.dlc << 16); // Set Data Length Code (DLC) in transmit frame info
 
-   void delay_millisec(unsigned int ms)
-{
-    T0PR=60000-1;//microsecs
-	T0TCR=0x01;
-	while(T0TC<ms);
-	T0TCR=0x03;
-	T0TCR=0x00;
+    if(m1.rtr == 0)          // If it is  data frame
+    {
+        C2TFI1 &= ~(1 << 30); // Clear RTR bit (RTR = 0)
+        C2TDA1  = m1.byteA;   // Load lower 4 bytes of data
+        C2TDB1  = m1.byteB;   // Load upper 4 bytes of data
+    }
+    else                     // If it is  remote frame
+    {
+        C2TFI1 |= (1 << 30);  // Set RTR bit (RTR = 1)
+    }
+
+    C2CMR = (1 << 0) | (1 << 5); // start transmission using Tx Buffer1
+
+    while((C2GSR & (1 << 3)) == 0); // Wait until transmission is complete
 }
 
-   	/*if(m1.id==0x02)
-			{ 
-				uart0_tx_string("speed check\r\n");
-			speed=m1.byteA;
-		sprintf((char*)arr, "speed=%02d", speed);   // avoid \r\n on LCD
-        uart0_tx_string((char*)arr);
-        uart0_tx_string("\r\n");
-		//delay_millisec(3000);
-		//uart0_tx_string("speed check\r\n");
-		   
-			}*/
