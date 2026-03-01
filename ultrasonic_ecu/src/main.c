3d490e4c3ffdbae1fe630f239c1f8e886bedb9c1
Naga Viswanth <132557170+nagabonthu@users.noreply.github.com>
2026-03-01 12:58:09 +0530

    Refactor ultrasonic distance calculation and CAN transmission

--- ultrasonic_ecu/src/main.c
+++ ultrasonic_ecu/src/main.c
@@ -1,78 +1,56 @@
-/** Ultrasonic sensor distance calculation transmit */
+/** Ultrasonic sensor distance calculation and CAN transmit to dashboard ECU */
 #include <lpc21xx.h>
 #include <stdio.h>
+#include "cantransmit.h" // CAN transmit header
 
-#include "cantransmit.h"
-#include "uartbaud.c"
+unsigned int ultrasonic_reading(void);//function declartion
 
-unsigned int ultrasonic_distance(void);
-
+//trigger and echo pins
 #define trig (1<<10) 
 #define echo (1<<11)
-
 								  
 int main()
 {
-
 unsigned int dist;
-char arr[20];
- CAN2_MSG m1;
- can2_init();
- uart0_init(11520);
- 
- uart0_tx_string("vishwa\r\n");
-
-
+CAN2_MSG m1;      // CAN message structure
+	
+can2_init();     // Initializing CAN controller	
  while(1)
  { 
-
-   dist=ultrasonic_distance();
-   can_delay(1000);
-    //sprintf((char*)arr, "distance=%02d", dist);   // avoid \r\n on LCD
-    //uart0_tx_string((char*)arr);
-    //uart0_tx_string("\r\n");
- 
-   m1.id=0x01;
-   m1.dlc=4;
-   m1.rtr=0;
-   m1.byteA=dist;
+   distance=ultrasonic_reading();  // function call to read distance from ultrasonic sensor 
+   delay_can(1000);               // Delay of 1000ms between transmissions
+   m1.id=0x01;   //Messgae ID
+   m1.dlc=4;     // data length code
+   m1.rtr=0;     // data frame
+   m1.byteA=dist;//measured distance storing in byteA
    m1.byteB=0;
-   can2_tx(m1);
-
-
-   
-
-
+   can2_tx(m1); // transmit CAN message
 
  } 
 }
-
-unsigned int ultrasonic_distance(void)
+/** Function to measure distance using ultrasonic sensor **/
+unsigned int ultrasonic_reading(void)
 {
+ unsigned int count=0;  
+ unsigned int dist=0;   
 
- unsigned int count=0;
-unsigned int distance=0;
+ IODIR0 |= trig;    // trigger pin as output
+ IODIR0 &= ~(echo); // echo pin as input
 
-IODIR0 |= trig;   //making trigger output
-IODIR0 &= ~(echo);  //making echo pin input
-
-IOCLR0=trig;
-delay_us(10);
-IOSET0=trig;	//sending 10 microseconds high pluse through trans pin
-delay_us(10);
-IOCLR0=trig;
-
-count=0;
-
-while (!(IOPIN0&echo));
-
-   T1PR=60-1;
-   T1TCR=0x01;
-while((IOPIN0&echo));
-count=T1TC;
-T1TCR=0x03;
-T1TCR=0x00;
-
-distance=count/58;  //formula for convert into distance
-return distance;
+ IOCLR0=trig;  // Clear trigger pin 
+ delay_us(10); 
+ IOSET0=trig;// Set trigger pin high
+ delay_us(10); // Keep high for 10 microseconds
+ IOCLR0=trig; // Clear trigger pin
+ 
+ while (!(IOPIN0&echo)); // Wait until echo pin goes high
+   T1PR=60-1;   // Prescaler for 1µs (PCLK=60MHz)
+   T1TCR=0x01; // Enable Timer1
+ while((IOPIN0&echo)); // Wait until echo pin goes low
+	
+ count=T1TC; // Capturing the timer count
+ T1TCR=0x03; // Stop and resetting Timer1
+ T1TCR=0x00;// Clear Timer1
+ dist=count/58;  //formula: distance(cm)= time/58;
+ return dist; // Returning measured distance
 }
