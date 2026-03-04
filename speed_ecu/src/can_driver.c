dc3ff02f69865a850c812a65c32f876325b99ebd
Naga Viswanth <132557170+nagabonthu@users.noreply.github.com>
2026-03-04 20:06:30 +0530

    Update can_driver.c

--- speed_ecu/src/can_driver.c
+++ speed_ecu/src/can_driver.c
@@ -1,74 +1,85 @@
-/** cn transmit****/
-/*can2_driver.c*/
+/* can2_driver.c
+   Simple CAN2 driver (init, transmit, receive) + Timer0 delay */
+
+#include <lpc21xx.h>
+
 typedef unsigned int u32;
+
+/* CAN message structure */
+typedef struct
+{
+    u32 id;      // CAN Identifier
+    u32 rtr;     // 0 = Data frame, 1 = Remote frame
+    u32 dlc;     // Data length (0 to 8)
+    u32 byteA;   // Data bytes 0-3
+    u32 byteB;   // Data bytes 4-7
+} CAN2_MSG;
+
+/* Function prototypes */
 void can2_init(void);
-void delay_millisec(unsigned int);
-typedef struct CAN2
-  {
-	u32 id;
-	u32 rtr;
-	u32 dlc;
-	u32 byteA;
-	u32 byteB;
- }CAN2_MSG;
 void can2_tx(CAN2_MSG m1);
-void can2_rx(CAN2_MSG*);
+void can2_rx(CAN2_MSG *m1);
+void delay_millisec(unsigned int ms);
+
+/* Initialize CAN2 (125 kbps @ PCLK = 60 MHz) */
 void can2_init(void)
- {
-	PINSEL1|=0x00014000;//P0.23-->RD2 & P0.24-->TD2
-	VPBDIV=1; //PCLK=60MHz
-    C2MOD=0x1; //CAN2 into Reset  Mode 
-	C2BTR=0x001C001D; //B125Kbps @ PLCK=60MHz	
-	AFMR=0x2;//Enable Acceptance Filter
-    C2MOD=0x0; //CAN2 into Normal Mode
- }
+{
+    PINSEL1 |= 0x00014000;   // P0.23 = RD2, P0.24 = TD2 (CAN2 pins)
+    VPBDIV   = 1;            // PCLK = CCLK (assumed 60 MHz)
+
+    C2MOD = 0x01;            // Reset mode 
+    C2BTR = 0x001C001D;      // Bit timing for 125 kbps @ 60 MHz PCLK
+    AFMR  = 0x02;            // Acceptance filter bypass (accept all)
+    C2MOD = 0x00;            // Normal mode (start CAN)
+}
+
+/* Transmit one CAN frame using TX Buffer 1 */
 void can2_tx(CAN2_MSG m1)
 {
-  C2TID1=m1.id;
-  C2TFI1=(m1.dlc<<16);	
-	if(m1.rtr==0) //if data frame
-	{
-		C2TFI1&=~(1<<30);//RTR=0
-		C2TDA1=m1.byteA; //lower 4bytes of data
-		C2TDB1=m1.byteB; //upper 4bytes of data
-	}
-	else
-	{
-		C2TFI1|=(1<<30); //RTR=1
-	}
-	C2CMR=(1<<0)|(1<<5);//Start Xmission & select Tx Buf1
-    while((C2GSR&(1<<3))==0);//wait for data Xmission.
-}
-void can2_rx(CAN2_MSG* m1)
-  {
-    while((C2GSR&0x1)==0);//waiting untill message received
-    m1->id=C2RID;
-	m1->dlc=(C2RFS>>16)&0xF;
-	m1->rtr=(C2RFS>>30)&0x1;
-	if(m1->rtr==0){ //if data frame
-		m1->byteA=C2RDA;
-		m1->byteB=C2RDB;
-	}
-   C2CMR=(1<<2);//free receiver buffer(imp)
-   }
+    C2TID1 = m1.id;          // Load CAN ID
+    C2TFI1 = (m1.dlc << 16); // Set DLC in frame info
 
-   void delay_millisec(unsigned int ms)
+    if (m1.rtr == 0)         // Data frame
+    {
+        C2TFI1 &= ~(1 << 30); // RTR = 0
+        C2TDA1  = m1.byteA;   // Data bytes 0-3
+        C2TDB1  = m1.byteB;   // Data bytes 4-7
+    }
+    else                     // Remote frame
+    {
+        C2TFI1 |= (1 << 30);  // RTR = 1
+    }
+
+    C2CMR = (1 << 0) | (1 << 5);     // Start TX + select TX buffer 1
+    while ((C2GSR & (1 << 3)) == 0); // Wait until transmission completes
+}
+
+/* Receive one CAN frame (polling) */
+void can2_rx(CAN2_MSG *m1)
 {
-    T0PR=60000-1;//microsecs
-	T0TCR=0x01;
-	while(T0TC<ms);
-	T0TCR=0x03;
-	T0TCR=0x00;
+    while ((C2GSR & 0x01) == 0);     // Wait until receive buffer has data
+
+    m1->id  = C2RID;                 // Read received ID
+    m1->dlc = (C2RFS >> 16) & 0x0F;  // Extract DLC
+    m1->rtr = (C2RFS >> 30) & 0x01;  // Extract RTR
+
+    if (m1->rtr == 0)                // If data frame, read payload
+    {
+        m1->byteA = C2RDA;           // Data bytes 0..3
+        m1->byteB = C2RDB;           // Data bytes 4..7
+    }
+
+    C2CMR = (1 << 2);                // Release receive buffer (important)
 }
 
-   	/*if(m1.id==0x02)
-			{ 
-				uart0_tx_string("speed check\r\n");
-			speed=m1.byteA;
-		sprintf((char*)arr, "speed=%02d", speed);   // avoid \r\n on LCD
-        uart0_tx_string((char*)arr);
-        uart0_tx_string("\r\n");
-		//delay_millisec(3000);
-		//uart0_tx_string("speed check\r\n");
-		   
-			}*/
+/*  millisecond delay using Timer0 */
+void delay_millisec(unsigned int ms)
+{
+    T0PR  = 60000 - 1;   // Prescaler 
+    T0TCR = 0x01;        // Start timer
+    while (T0TC < ms);   // Wait
+    T0TCR = 0x03;        // Reset timer
+    T0TCR = 0x00;        // Stop timer
+}
+
+
